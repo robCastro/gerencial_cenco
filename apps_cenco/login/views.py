d5c9c5cbbe18545fc47c587e5847fc9631b7e8d7
Katherine <katcerna24@gmail.com>
2019-06-05 11:03:15 -0600

    proyecto inicial

---
+++ apps_cenco/login/views.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.http import Http404, HttpResponseRedirect, HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
