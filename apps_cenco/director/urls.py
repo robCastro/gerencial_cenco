9ab5bd0aa2512bfc3acf739ba4e316134fcc93ee
roberto <roberto.ernesto.castro@gmail.com>
2019-06-09 20:30:37 -0600

    Agregados archivo gitignore para ignorar archivos pyc, agregado bloque main_content en base.html

--- apps_cenco/director/urls.py
+++ apps_cenco/director/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from apps_cenco.director import views
 
 urlpatterns = [
-
+	url(r'^ingreso_retiros_estudiantes/$', views.verIngresosRetirosEstudiantes, name="ingreso_retiros_estudiantes"),
 ]
--- apps_cenco/director/views.py
+++ apps_cenco/director/views.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+def verIngresosRetirosEstudiantes(request):
+	context = {}
+	return render(request, 'director/ingresos-retiros-estudiantes.html', context)
