6358b3a48e9497f6f2b5fdb9d5bbdafb226d5b6b
roberto <roberto.ernesto.castro@gmail.com>
2019-06-15 08:24:58 -0600

    Pantallas de Entrada y Salida listas

--- apps_cenco/director/urls.py
+++ apps_cenco/director/urls.py
@@ -3,4 +3,23 @@ from apps_cenco.director import views
 
 urlpatterns = [
 	url(r'^ingreso_retiros_estudiantes/$', views.verIngresosRetirosEstudiantes, name="ingreso_retiros_estudiantes"),
+	url(r'^desempenio_didactico/$', views.verDesempenioDidactico, name="desempenio_didactico"),
+	url(r'^ingreso_econ_suc/$', views.verIngresoEconSuc, name="ingreso_econ_suc"),
+	url(r'^desempenio_sucursal/$', views.verDesempenioSucursal, name="desempenio_sucursal"),
+
+
+
+
+	url(r'^salida_ingreso_retiros_estudiantes/$',
+		views.verSalidaIngresosRetirosEstudiantes,
+		name="sal_ingreso_retiros_estudiantes"),
+
+	url(r'^salida_desempenio_sucursal/$',
+		views.verSalidaDesempenioSucursal,
+		name="sal_desempenio_sucursal"),
+
+	url(r'^salida_ingreso_econ_suc/$',
+		views.verSalidaIngresoEconSuc,
+		name="sal_ingreso_econ_suc"),
+	
 ]
--- apps_cenco/director/views.py
+++ apps_cenco/director/views.py
@@ -3,8 +3,64 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from datetime import datetime
+
 # Create your views here.
 
 def verIngresosRetirosEstudiantes(request):
-	context = {}
+	fechaHoy = str((datetime.now().date().strftime("%m/%d/%Y")))
+	#fechaInicio = datetime.strptime(datetime.now().date() - timedelta(days = 7), "%d/%m/%Y").date()
+	context = {
+		'fechaHoy' : fechaHoy,
+		#'fechaInicio' : fechaInicio,
+	}
 	return render(request, 'director/ingresos-retiros-estudiantes.html', context)
+
+def verDesempenioDidactico(request):
+	fechaHoy = str((datetime.now().date().strftime("%m/%d/%Y")))
+	context = {
+		'fechaHoy' : fechaHoy,
+	}
+	return render(request, 'director/desempenio-didactico.html', context)
+
+def verDesempenioSucursal(request):
+	fechaHoy = str((datetime.now().date().strftime("%m/%d/%Y")))
+	context = {
+		'fechaHoy' : fechaHoy,
+	}
+	return render(request, 'director/desempenio-sucursal.html', context)
+
+def verIngresoEconSuc(request):
+	fechaHoy = str((datetime.now().date().strftime("%m/%d/%Y")))
+	context = {
+		'fechaHoy' : fechaHoy,
+	}
+	return render(request, 'director/ingresos-econ-suc.html', context)
+
+
+
+#Salidas
+
+
+def verSalidaIngresosRetirosEstudiantes(request):
+	fechaHoy = str((datetime.now().date().strftime("%m/%d/%Y")))
+	#fechaInicio = datetime.strptime(datetime.now().date() - timedelta(days = 7), "%d/%m/%Y").date()
+	context = {
+		'fechaHoy' : fechaHoy,
+		#'fechaInicio' : fechaInicio,
+	}
+	return render(request, 'director/sal-ingresos-retiros-estudiantes.html', context)
+
+def verSalidaDesempenioSucursal(request):
+	fechaHoy = str((datetime.now().date().strftime("%m/%d/%Y")))
+	context = {
+		'fechaHoy' : fechaHoy,
+	}
+	return render(request, 'director/sal-desempenio-sucursal.html', context)
+
+def verSalidaIngresoEconSuc(request):
+	fechaHoy = str((datetime.now().date().strftime("%m/%d/%Y")))
+	context = {
+		'fechaHoy' : fechaHoy,
+	}
+	return render(request, 'director/sal-ingresos-econ-suc.html', context)
