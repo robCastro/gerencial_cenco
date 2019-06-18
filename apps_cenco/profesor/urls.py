ae40398ac50062290e44d5292fde3302a71b4082
Patricia Solano <josselynsolano96@gmail.com>
2019-06-18 13:06:26 -0600

    html desempeño estudiantil

--- apps_cenco/profesor/urls.py
+++ apps_cenco/profesor/urls.py
@@ -2,5 +2,16 @@ from django.conf.urls import url
 from apps_cenco.profesor import views
 
 urlpatterns = [
+    
+#Entradas
+	url(r'^desempenio_estudiantil/$', views.verDesempenioEstudiantil, name="desempenio_estudiantil"),
+
+
+#Salidas
+	url(r'^salida_desempenio_estudiantil/$', views.verSalDesempenioEstudiantil, name="sal_desempenio_estudiantil"),
+
+
+#PDFS
+	url(r'^pdf_desempenio_estudiantil/$', views.RepDesempenioEstudiantil.as_view(), name='pdf_desempenio_estudiantil'),
 
 ]
--- apps_cenco/profesor/views.py
+++ apps_cenco/profesor/views.py
@@ -3,4 +3,41 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from datetime import datetime
+
+from django.views.generic import DetailView
+from wkhtmltopdf.views import PDFTemplateView, PDFTemplateResponse
+
+
 # Create your views here.
+
+
+#Entradas
+
+def verDesempenioEstudiantil(request):
+	return render(request, 'profesor/desempenio-estudiantil.html')
+
+
+#Salidas
+
+def verSalDesempenioEstudiantil(request):
+	fechaHoy = str((datetime.now().date().strftime("%d/%m/%Y")))
+	context = {
+		'fechaHoy' : fechaHoy,
+	}
+	return render(request, 'profesor/sal-desempenio-estudiantil.html', context)
+
+
+#Reportes
+
+class RepDesempenioEstudiantil(PDFTemplateView):
+    filename = 'my_pdf.pdf'
+    template_name = 'profesor/rep-desempenio-estudiantil.html'
+    show_content_in_browser = True  ###Para no descargar automaticamente
+
+    ###Para agregar context manuales
+    def get_context_data(self, **kwargs):
+        context = super(RepDesempenioEstudiantil, self).get_context_data(**kwargs)
+        context['fechaHoy'] = str((datetime.now().date().strftime("%d/%m/%Y")))
+        return context
+
